1d99bbe70f78b4a78a78f4fb1cc6bb1dbc7ec3df
ZhengYuang <rion.z924@gmail.com>
2019-05-10 09:38:31 +0800

    Initial Commit

---
+++ MyQQ/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyQQ
+//
+//  Created by Rion on 2019/5/10.
+//  Copyright © 2019 Rion. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyQQ/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyQQ
+//
+//  Created by Rion on 2019/5/10.
+//  Copyright © 2019 Rion. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
