e51580d290e49ee66a6c1fb9bcd13c92e9d9cfd7
ZhengYuang <rion.z924@gmail.com>
2019-05-20 17:49:12 +0800

    setting window

---
+++ MyQQ/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  MyQQ
+//
+//  Created by Rion on 2019/5/20.
+//  Copyright © 2019 Rion. All rights reserved.
+//
+
+import Cocoa
+
+class SettingViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
--- MyQQ/ViewController.swift
+++ MyQQ/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: NSViewController {
     @IBOutlet weak var accountField: LoginAccountField!
     @IBOutlet weak var pwdField: LoginPwdField!
     @IBOutlet weak var loginButton: NSButton!
+    @IBOutlet weak var arrowButton: NSButton!
+    
+    lazy var settingWindow: NSWindow = {
+        let window = NSWindow(contentRect: NSMakeRect((view.window?.frame.origin.x)!, (view.window?.frame.origin.y)! - 100, (view.window?.frame.size.width)!, 100), styleMask: [.titled], backing: NSWindow.BackingStoreType.buffered, defer: true)
+        window.contentViewController = NSStoryboard(name: NSStoryboard.Name.init("Main"), bundle: nil).instantiateController(withIdentifier: NSStoryboard.SceneIdentifier.init("settingVC")) as! SettingViewController
+        view.window?.addChildWindow(window, ordered: .below)
+        return window
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +29,27 @@ class ViewController: NSViewController {
                 self.loginButton.isEnabled = !self.accountField.stringValue.isEmpty && !self.pwdField.stringValue.isEmpty
         }
 
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        
+//        self.settingWindow.setFrame(NSMakeRect((self.view.window?.frame.origin.x)!, (self.view.window?.frame.origin.y)! + 22, (self.view.window?.frame.size.width)!, 0), display: false)
+//        self.view.window?.addChildWindow(self.settingWindow, ordered: .below)
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        
+        accountField.becomeFirstResponder()
+        NotificationCenter.default.addObserver(forName: NSWindow.willMoveNotification, object: nil, queue: OperationQueue.main) { (notifi) in
+            if self.arrowButton.state == .off {
+                self.settingWindow.setFrame(NSMakeRect((self.view.window?.frame.origin.x)!, (self.view.window?.frame.origin.y)! + 22, (self.view.window?.frame.size.width)!, 0), display: false)
+                self.view.window?.addChildWindow(self.settingWindow, ordered: .below)
+            }
+        }
+        
+        
     }
 
     override var representedObject: Any? {
@@ -38,7 +67,22 @@ class ViewController: NSViewController {
     @IBAction func onLoginClick(_ sender: NSButton) {
     }
     
+    @IBAction func onArrowClick(_ sender: NSButton) {
+        
+        NSAnimationContext.runAnimationGroup { (context) in
+            context.duration = 0.45
+            context.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+            if sender.state == .on {
+                settingWindow.animator().setFrame(NSMakeRect((view.window?.frame.origin.x)!, (view.window?.frame.origin.y)! - 78, (view.window?.frame.size.width)!, 100), display: true)
+                
+                view.window?.addChildWindow(settingWindow, ordered: .below)
+            } else {
+                //            view.window?.removeChildWindow(settingWindow!)
+                settingWindow.animator().setFrame(NSMakeRect((view.window?.frame.origin.x)!, (view.window?.frame.origin.y)! + 22, (view.window?.frame.size.width)!, 0), display: false)
+            }
+        }
+        
+    }
     
-
 }
 
